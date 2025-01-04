c1456d70ef55b37ebe051d1eda11ef5e63058fb3
Lachlan Orton <56850750+lachlanorton@users.noreply.github.com>
2025-01-04 13:44:17 +1100

    Fixed bug with player unable to take subsequent damage. Cleaned up code and added comments.
    Bug was due to poor logic on animation handling. Was checking "if player wasn't being hit OR player wasn't healing", rather than AND.

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,24 +2,9 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-#	boundaryType = self.get_node("CollisionShape2D")
-#	if boundaryType is worldboundary, then body.playerVoid() otherwise body.playerHit()
 	if self.name == "WorldVoid":
-		body.playerVoid()
+		body.playerDied()
 	else:
 		body.playerHit()
 
-# old death mechanism
-# new death mechanism moved to player.gd script
-
-# func _on_body_entered(body: Node2D) -> void:
-#	Engine.time_scale = 0.5
-#	body.playerDied()
-#	body.get_node("CollisionShape2D").queue_free()
-#	var death_label = body.get_node("Camera2D/DeathLabel")
-#	death_label.visible = true
-#	timer.start()
-
-# func _on_timer_timeout() -> void:
-#	Engine.time_scale = 1
-#	get_tree().reload_current_scene()
+# death function moved to player.gd
--- scripts/player.gd
+++ scripts/player.gd
@@ -12,6 +12,9 @@ var is_healing = false
 @onready var reset_game_timer: Timer = %ResetGameTimer
 @onready var health_bar: AnimatedSprite2D = $HealthBar
 
+# This function handles player death. Either called from here when player health reaches 0
+# or called from killzone.gd when player enters WorldVoid.
+# Slows game time, plays death animation, shows label, then starts reset game timer.
 func playerDied():
 	died = true
 	Engine.time_scale = 0.5
@@ -22,21 +25,24 @@ func playerDied():
 	death_label.visible = true
 	reset_game_timer.start()
 
+# This function handles player taking damage. 
+# It is called from killzone.gd when the killzone is not a WorldVoid.
 func playerHit():
 	if is_being_hit:
 		return
 	is_being_hit = true
 	health -= 1
-	animation_player.play("audio_hit")
-	animated_sprite.play("hit")
-	var hit_anim_fin = animated_sprite.animation_finished
-	health_bar.set_frame(int(health))
 	if health <= 0:
 		playerDied()
 	else:
+		animation_player.play("audio_hit")
+		animated_sprite.play("hit")
+		var hit_anim_fin = animated_sprite.animation_finished
+		health_bar.set_frame(int(health))
 		await(hit_anim_fin)
 		is_being_hit = false
 
+# This function handles player healing. It is called from fruit.gd when body collides with fruit.
 func playerHeal():
 	if is_healing:
 		return
@@ -45,16 +51,18 @@ func playerHeal():
 		return
 	else:
 		health += 1
+		
+	# Update health bar animated sprite
 	health_bar.set_frame(int(health))
+	
+	# Run heal animation, wait until animation is finished, then continue
+	# This allows for heal animation to run, rather than being overruled by idle/jump/run animations
 	animated_sprite.play("heal")
 	var heal_anim_fin = animated_sprite.animation_finished
 	await(heal_anim_fin)
 	is_healing = false
 
-func playerVoid():
-	health == 0
-	playerDied()
-
+# This function handles player movement and animation.
 func _physics_process(delta: float) -> void:
 	if died:
 		# Stop player momentum
@@ -64,19 +72,19 @@ func _physics_process(delta: float) -> void:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
 
-		# Handle jump.
+		# Handle jump and related sfx.
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			animation_player.play("anim_jump")
 
 		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		# move right = direction = 1
-		# move left = direction = -1
+		# move right means direction = 1
+		# move left means direction = -1
 		var direction := Input.get_axis("move_left", "move_right")
 	
 		# update sprite animation based on action
-		if !is_being_hit or !is_healing:
+		# First check that player is not currently being hit or healing
+		if !is_being_hit and !is_healing:
 			if is_on_floor():
 				if direction == 0:
 					animated_sprite.play("idle")
@@ -91,18 +99,20 @@ func _physics_process(delta: float) -> void:
 		if direction < 0:
 			animated_sprite.flip_h = true
 	
-			
+		# If a direction is being input, move player in that direction	
 		if direction:
 			velocity.x = direction * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-	move_and_slide()
+		
+		move_and_slide()
 	
 
-
+# This function runs after the reset_game_timer has completed. It restarts the level.
 func _on_reset_game_timer_timeout() -> void:
 	Engine.time_scale = 1
 	get_tree().reload_current_scene()
 
+# When player loads in, health bar animated sprite is set to current health
 func _on_ready() -> void:
 	health_bar.set_frame(int(health))
